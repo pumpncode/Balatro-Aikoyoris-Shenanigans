0de792d36f8109274d91c1f41bef01a68f16e453
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2025-03-04 23:08:48 +0700

    not just yet compat

---
+++ modules/compat/notjustyet.lua
@@ -0,0 +1,21 @@
+if G.FUNCS.njy_endround then
+    G.E_MANAGER:add_event(Event{
+        trigger="after",
+        func = function ()
+            local notjustyetCanEndRoundHook = G.FUNCS.njy_can_endround
+            G.FUNCS.njy_can_endround = function (e)
+                if (G.GAME.current_round.advanced_blind) then 
+                    if G.GAME.aiko_puzzle_win then
+                        e.config.colour = G.C[G.njy_colour]
+                        e.config.button = 'njy_attempt_endround'
+                    else
+                        e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+                        e.config.button = nil
+                    end
+                else
+                    local r = notjustyetCanEndRoundHook(e)
+                end
+            end
+        end
+    })
+end

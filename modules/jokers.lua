54fce2abab9f7a96307a56ed0d66b1b5112f0974
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2025-03-14 02:39:05 +0700

    in case misprint randomizes it

--- modules/jokers.lua
+++ modules/jokers.lua
@@ -876,6 +876,7 @@ SMODS.Joker{
     end,
     calculate = function (self, card, context)
         if context.end_of_round and context.cardarea == G.jokers then
+            card.ability.akyrs_cycler = math.floor(card.ability.akyrs_cycler)
             card.ability.akyrs_priority_draw_rank = nil
             card.ability.akyrs_priority_draw_suit = nil
             card.ability.akyrs_priority_draw_conditions = nil
